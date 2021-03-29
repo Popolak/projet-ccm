e8b2dee1b033d2de58be79300c91a5cf36d3ba0c
Foxouille <55891646+Foxouille@users.noreply.github.com>
2021-03-29 11:51:02 +0200

    gestion changement de perso
    j'ai remplacé la plupart des "Tom" par "perso_control[roulette]"

--- src/main.c
+++ src/main.c
@@ -74,11 +74,20 @@ void menu_accueil(){//menu d'accueil du jeu
 
 
 int main(int argc, char* argv[]){
+
+	menu_accueil(); // le programme continue uniquement s'il passe le menu
+
 	int WINW=1280, WINH=720,x=CHUNKH-CHUNKH*ratioSol-30,y=TAILLE_MUR+40, nbText;
     chunk_t * chunk;
     niveau_t * niv=NULL;
     salle_t * salle=NULL;
     perso_t *Tom=NULL;
+    perso_t *Carot=NULL;
+    perso_t *viande=NULL;
+    perso_t *perso_control[3];
+    int roulette=0;
+    int i=0;
+    int morts=0;
     entite_t * entite_test=NULL;
     SDL_Window * win=NULL;
     SDL_Renderer * ren=NULL;
@@ -110,8 +119,17 @@ int main(int argc, char* argv[]){
     SDL_Texture * murTexture=creer_texture_image(ren, "../graphics/texture/room_textures/texture_mur.bmp");
     SDL_Texture ** joueurTextures=NULL, **entite_test_textures;
 
+    //A ajouter: textures pour carotte et viande
     joueurTextures=creer_tableau_textures(ren,&nbText,"../graphics/sprite/personnage_sprites/Tom vrai neutre.png","../graphics/sprite/personnage_sprites/Tom marche 1.png","../graphics/sprite/personnage_sprites/Tom marche 2.png","");
-    Tom=perso_creer("Tom","tomate",30,salle,chunk,pos,0,0,300,700,60,80,30,60,0.18,0,0,nbText,joueurTextures);
+    //Tomate est le premier personnage que l'on contrôle
+    Tom=perso_creer("Tom","tomate",40,salle,chunk,pos,0,0,300,700,60,80,30,60,0.18,0,0,nbText,joueurTextures);
+    Carot=perso_creer("Carot","carotte",30,NULL,NULL,pos,0,0,300,600,60,80,30,60,0.17,0,0,nbText,joueurTextures);
+    Viande=perso_creer("Viande","viande",60,NULL,NULL,pos,0,0,300,500,60,80,30,60,0.15,0,0,nbText,joueurTextures);
+
+    //servira au changement de perso
+    perso_control[0] = Tom;
+    perso_control[1] = Carot;
+    perso_control[2] = Viande;
 
     entite_test_textures=creer_tableau_textures(ren,&nbText,"../graphics/sprite/entite_sprites/test.png","");
     printf("%d\n", nbText);
@@ -123,6 +141,8 @@ int main(int argc, char* argv[]){
         SDL_DestroyTexture(murTexture);
         SDL_DestroyTexture(bgTexture);
         Tom->detruire_perso(&Tom);
+        Carot->detruire_perso(&Carot);
+        Viande->detruire_perso(&Viande);
         entite_test->detruire_ent(&entite_test);
         SDL_DestroyRenderer(ren);
         SDL_DestroyWindow(win); 
@@ -139,19 +159,33 @@ int main(int argc, char* argv[]){
         secMaint=1.0*SDL_GetTicks()/1000;
         sec=secMaint-secAvant;
         secDeg+=sec;
-        Tom->lastSprite+=sec;
+        perso_control[roulette]->lastSprite+=sec;
         secAvant=secMaint;
         if(sec>0.001){
-            Tom->deplacer((entite_t *)Tom,sec);
+            perso_control[roulette]->deplacer((entite_t *)perso_control[roulette],sec);
             entite_test->deplacer(entite_test,sec);
         }
-        if(Tom->contact((entite_t*) Tom, entite_test) && secDeg > secInvins){
-            Tom->prendre_coup(Tom,10);
-            printf("Tom prend un coup %d\n", Tom->vie);
-            if(Tom->vie<=0)
-                run=SDL_FALSE;
+        if(perso_control[roulette]->contact((entite_t*) perso_control[roulette], entite_test) && secDeg > secInvins){
+            perso_control[roulette]->prendre_coup(perso_control[roulette],10);
+            printf("Le perso prend un coup %d\n", perso_control[roulette]->vie);
+            if(en_vie(perso_control[roulette] == FAUX)){ //Si le personnage meurt on le remplace par le prochain dans la liste
+               	morts++;
+                i = roulette;
+                roulette++;
+                if(roulette > 2)
+                	roulette = 0;
+                perso_copie_partiel(perso_control[roulette], perso_control[i]);
+                perso_depop(perso_depop[i]);
+            }
+            if(morts == 3)
+            	run=SDL_FALSE;
             secDeg=0;
         }
+
+        /*if(ressusiter perso_control[i]){ // théorique
+        	morts--;
+   		 }*/
+
         while(SDL_PollEvent(&events)){
             switch (events.type)
             {
@@ -169,6 +203,18 @@ int main(int argc, char* argv[]){
                 }
                 if (events.key.keysym.sym == SDLK_SPACE){ 
                     tot_key = tot_key | KEY_JUMP;
+                }
+                if (events.key.keysym.sym == SDLK_e){//changement du personnage qu'on controle 
+                	if(morts<2){
+                		i = roulette;
+                		roulette++;
+                		if(roulette > 2)
+                			roulette = 0;
+                		if(en_vie(perso_control[roulette] == FAUX))
+                			roulette++;
+                		perso_copie_partiel(perso_control[roulette], perso_control[i]);
+                		perso_depop(perso_depop[i]);
+                	}
                 }
 
                 break;
@@ -188,14 +234,16 @@ int main(int argc, char* argv[]){
         
         SDL_RenderClear(ren);
         SDL_RenderCopy(ren,bgTexture,NULL,NULL);
-        Tom->update_speed(Tom, tot_key);
-        Tom->afficher_chunk(ren,(entite_t*)Tom,WINH,WINW);
+        perso_control[roulette]->update_speed(perso_control[roulette], tot_key);
+        perso_control[roulette]->afficher_chunk(ren,(entite_t*)perso_control[roulette],WINH,WINW);
         entite_test->afficher_chunk(ren, entite_test,WINH,WINW);
 
-        Tom->hitbox(ren,Tom,WINH,WINW);
+        perso_control[roulette]->hitbox(ren,perso_control[roulette],WINH,WINW);
         entite_test->hitbox(ren, entite_test,WINH,WINW);
-        if(render_mur_chunk(ren,murTexture,Tom->chunk,WINW,WINH )==ERR_DEB_MEMOIRE){
+        if(render_mur_chunk(ren,murTexture,perso_control[roulette]->chunk,WINW,WINH )==ERR_DEB_MEMOIRE){
             Tom->detruire_perso(&Tom);
+            Carot->detruire_perso(&Carot);
+            Viande->detruire_perso(&Viande);
             entite_test->detruire_ent(&entite_test);
             niv->detruire(&niv);
             SDL_DestroyTexture(bgTexture);
@@ -209,6 +257,8 @@ int main(int argc, char* argv[]){
     }
     
     Tom->detruire_perso(&Tom);
+    Carot->detruire_perso(&Carot);
+    Viande->detruire_perso(&Viande);
     entite_test->detruire_ent(&entite_test);
     niv->detruire(&niv);
     SDL_DestroyTexture(bgTexture);
